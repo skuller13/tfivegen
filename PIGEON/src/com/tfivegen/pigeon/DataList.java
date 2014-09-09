bbd01b080db08181032b03178797571ff741e8a5
skuller13 <skuller13@gmail.com>
2014-09-10 00:49:59 +0700

    Dynamic content on dialog extended

--- PIGEON/src/com/tfivegen/pigeon/DataList.java
+++ PIGEON/src/com/tfivegen/pigeon/DataList.java
@@ -1,12 +1,17 @@
 package com.tfivegen.pigeon;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.app.ProgressDialog;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
@@ -19,11 +24,22 @@ import com.tfivegen.pigeon.listviewadaper.FetchDataTask;
 public class DataList extends Activity implements FetchDataListener {
 	private ProgressDialog dialog;
 	ListView list;
+	
+	Bitmap bmScreen;
+	 
+	Dialog screenDialog;
+	static final int ID_SCREENDIALOG = 1;
+	 
+	ImageView bmImage;
+	TextView TextOut;
+	View screen;
+	Button btnScreenDialog_OK;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_data_list);
-		
+		screen = (View)findViewById(R.id.screen);
         list=(ListView)findViewById(R.id.list);
         initView();
 	}
@@ -48,6 +64,10 @@ public class DataList extends Activity implements FetchDataListener {
         list.setOnItemClickListener(new OnItemClickListener(){
         	public void onItemClick(AdapterView<?> parent, View view,int position, long id) {
         		
+        		screen.setDrawingCacheEnabled(true);
+        		bmScreen = screen.getDrawingCache();
+        		showDialog(ID_SCREENDIALOG);
+        		   
         	    Toast.makeText(getApplicationContext(),
         	      "You've selected this job : " + data.get(position).getTitle() , Toast.LENGTH_LONG)
         	      .show();
@@ -63,6 +83,41 @@ public class DataList extends Activity implements FetchDataListener {
         Toast.makeText(this, msg, Toast.LENGTH_LONG).show();        
     }
     
+    @Override
+    protected Dialog onCreateDialog(int id) {
+     // TODO Auto-generated method stub
+     
+     screenDialog = null;
+     switch(id){
+     case(ID_SCREENDIALOG):
+      screenDialog = new Dialog(this);
+      screenDialog.setContentView(R.layout.dialog);
+      bmImage = (ImageView)screenDialog.findViewById(R.id.image);
+      TextOut = (TextView)screenDialog.findViewById(R.id.textout);
+      btnScreenDialog_OK = (Button)screenDialog.findViewById(R.id.okdialogbutton);
+      btnScreenDialog_OK.setOnClickListener(btnScreenDialog_OKOnClickListener);
+     }
+     return screenDialog;
+    }
     
-
+    @Override
+    protected void onPrepareDialog(int id, Dialog dialog) {
+     // TODO Auto-generated method stub
+     switch(id){
+     case(ID_SCREENDIALOG):
+      dialog.setTitle("Captured Screen");
+      TextOut.setText("Hello");
+      bmImage.setImageBitmap(bmScreen);
+      break;
+     }
+    }
+     
+    private Button.OnClickListener btnScreenDialog_OKOnClickListener
+     = new Button.OnClickListener(){
+     
+     @Override
+     public void onClick(View arg0) {
+      // TODO Auto-generated method stub
+      screenDialog.dismiss();
+     }};
 }
