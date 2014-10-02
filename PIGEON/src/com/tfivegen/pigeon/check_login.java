972d7b3faf956cab2b6ead28be74709ea038fc6d
ttin_000 <ttin_000@Goomgum>
2014-10-02 21:33:51 +0700

    add activity profile

---
+++ PIGEON/src/com/tfivegen/pigeon/check_login.java
@@ -0,0 +1,19 @@
+package com.tfivegen.pigeon;
+
+public class check_login {
+	static String id = null;
+	static String username = null;
+	static String password = null;
+	static String email = null;
+	static String phone = null;
+	static String error_status = null;
+	public static void logout()
+	{
+		 id = null;
+		 username = null;
+		 password = null;
+		 email = null;
+		 phone = null;
+		 error_status = null;
+	}
+}
